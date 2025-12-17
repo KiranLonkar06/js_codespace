e405dc4fe8348db2ab893c7e6ad0a5dd18b54209
Kiran Lonkar <176193317+KiranLonkar06@users.noreply.github.com>
2025-12-17 13:52:49 +0000

    done with prefix and postfix

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -18,3 +18,31 @@ let someNumber = 33;
 
 let stringNumber = String(someNumber);
 console.log(stringNumber)
+
+
+//      OPERATION
+
+
+let value=3;
+let negativeVal=-value;
+console.log(negativeVal);
+// console.log(2+2*3)
+// console.log(3%5)
+let str1="hello";
+let str2=" world";
+let str3=str1+str2;
+// console.log(str3)
+// console.log("2" + 4)
+// console.log("2"+3+4)
+// console.log(2+4+"5")
+
+// console.log("");
+
+
+let num1,num2,num3;
+num1=num2=num3=3;
+console.table([num1,num2,num3]);
+
+let gameCounter = 100;
+
+console.log(++gameCounter)
