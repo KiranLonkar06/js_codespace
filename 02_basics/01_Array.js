a05e0098e8440d31a9a43a2f77fa56ff9f753bf2
Kiran Lonkar <176193317+KiranLonkar06@users.noreply.github.com>
2025-12-19 09:08:45 +0000

    started with dom

---
+++ 02_basics/01_Array.js
@@ -0,0 +1,9 @@
+//Array
+const myArray = [0,1,2,3,4,5,6,7];
+console.log(myArray[4]);
+const myArray2 =  new Array(1,"Kiran",2,3,4,5,6,7);
+console.log(myArray2);
+//  array methods 
+myArray2.push(9);
+console.log(myArray2)
+
