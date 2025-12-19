1f9c96f9c12a8bc1a93ba8da53a8d673db451892
Kiran Lonkar <176193317+KiranLonkar06@users.noreply.github.com>
2025-12-19 06:18:56 +0000

    Math Library done

--- 01-basics/06_Nums_Math.js
+++ 01-basics/06_Nums_Math.js
@@ -1,5 +1,27 @@
 const score = 400;
  
 const balance = new Number(100);
-console.log(balance);
-console.log(score);
+// console.log(balance);
+// console.log(score);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+const othernumber=45.4445;
+// console.log(othernumber.toPrecision(6));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+//    maths
+console.log(Math.PI);
+console.log(Math.cos(45));
+console.log(Math.abs(-3));
+console.log(Math.round(45.097));
+console.log(Math.pow(4,3));
+console.log(10*Math.random());
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max-min+1) )+min ) ;
